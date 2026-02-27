429dffbd35950dcf16afd7de04421fcd32e02a81
fortune-c <fortunate.adesinas@gmail.com>
2026-02-27 11:20:01 +0100

    average mark calculation

--- src/utils.c
+++ src/utils.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "../include/student.h"
 #include "../include/utils.h"
 
+extern Student *students;
+extern int studentCount;
+
 void calculateAverageMarks() {
-    printf("Utils");
+    if (studentCount == 0) {
+        printf("No students found.\n");
+        return;
+    }
+
+    float total = 0;
+    for (int i = 0; i < studentCount; i++) {
+        total += students[i].marks;
+    }
+
+    printf("Average marks is %.2f.\n", total / studentCount);
 };
 void sortStudents(int ascending) {
     printf("Utils");
